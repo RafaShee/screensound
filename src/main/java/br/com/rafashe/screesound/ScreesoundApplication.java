c50861af91dbdb9b6c4cba1f7f6a202e07355277
Rafaela Delgado <rafaelad054@gmail.com>
2025-03-04 10:44:27 -0300

    first commit

---
+++ src/main/java/br/com/rafashe/screesound/ScreesoundApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafashe.screesound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreesoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreesoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafashe/screesound/ScreesoundApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafashe.screesound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreesoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
